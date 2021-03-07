6394d096be3282635d5abcf90e19e3f8a92e6df6
puneet sharma <puneet8085@gmail.com>
2021-03-07 16:05:01 +0530

    commiting beforeTest in PuneetFIrstbranch

--- src/Class1.java
+++ src/Class1.java
@@ -1,8 +1,15 @@
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Class1 {
+	
+	@BeforeTest
+	public void beforTest()
+	{
+		System.out.println("Execute before @BeforeMetod now");
+	}
 	
 	@BeforeMethod
 	public void beforeMethod()
