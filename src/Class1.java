b6f9ff77cda1f9a8bc3356f2758c6727f298d987
puneet sharma <puneet8085@gmail.com>
2021-03-07 20:41:53 +0530

    having only @test in master

--- src/Class1.java
+++ src/Class1.java
@@ -1,37 +1,9 @@
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+
 import org.testng.annotations.Test;
 
 public class Class1 {
 	
-	@BeforeSuite
-	public void beforeSuite()
-	{
-		System.out.println("Execute before @BeforeTestnow");
-	}
 	
-	
-	@BeforeTest
-	public void beforTest()
-	{
-		System.out.println("Execute before @BeforeMetod now");
-	
-	}
-	
-	@AfterSuite
-	public void afterSuite()
-	{
-		System.out.println("Execute After @After Test");
-	}
-	
-	@BeforeMethod
-	public void beforeMethod()
-	{
-		System.out.println("Execute before @Test");
-	}
 	
 	@Test
 	public void testMethod()
@@ -39,10 +11,4 @@ public class Class1 {
 		System.out.println("Test method ");
 	}
 	
-
-	@AfterMethod
-	public void afterMethod()
-	{
-		System.out.println("Execute after @Test");
-	}
 }
