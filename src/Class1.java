028d7ccc0a042d64b40a07a7c545e24db55d4dcf
puneet sharma <puneet8085@gmail.com>
2021-03-07 20:57:31 +0530

    added new method aftermethod and not having beforemethod

--- src/Class1.java
+++ src/Class1.java
@@ -1,17 +1,21 @@
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Class1 {
 	
 	
-	@BeforeMethod
-	public void beforeMethod()
+	
+	@Test
+	public void testMethod()
 	{
 		System.out.println("Test method ");
 	}
-	@Test
-	public void testMethod()
+	
+	
+	@AfterMethod
+	public void afterMethod()
 	{
 		System.out.println("Test method ");
 	}
